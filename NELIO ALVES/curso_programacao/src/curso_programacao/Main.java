03b2dc21d5913f1446282dd4aa9c12e88c69762d
vallzin <valmilson@gmail.com>
2024-11-05 21:32:56 -0300

    saida de dados

--- NELIO ALVES/curso_programacao/src/curso_programacao/Main.java
+++ NELIO ALVES/curso_programacao/src/curso_programacao/Main.java
@@ -1,5 +1,35 @@
 package curso_programacao;
 
+import java.util.Locale;
+
+/**
+ * 
+ */
 public class Main {
+	
+	public static void main(String[] args) {
+		
+		int y = 32;
+		
+		String nome = "Maria";
+		int idade = 32;
+		double renda = 5000.0;
+	
+		System.out.println("Olá mundo!");
+		System.out.println("Bom dia!");
+		System.out.println(y);
+		System.out.println();
+		double x = 10.356879;
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		System.out.println("RESULTADO = " + x + "METROS");
+		System.out.printf("RESULTADO = %.2f metros%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		
+		
+		
+	}
 
 }
