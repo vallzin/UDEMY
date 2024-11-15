41d97ebcc8714030ff8c66d388f4267aef000e85
vallzin <valmilson@gmail.com>
2024-11-15 08:56:51 -0300

    seção10 exercícios 'media pares'

--- NELIO ALVES/secao10/src/secao10/MediaPares.java
+++ NELIO ALVES/secao10/src/secao10/MediaPares.java
@@ -3,7 +3,7 @@ package secao10;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class AbaixoDaMedia {
+public class MediaPares {
 
 	public static void main(String[] args) {
 		
@@ -13,35 +13,37 @@ public class AbaixoDaMedia {
 		System.out.print("Quantos elementos vai ter o vetor? ");
 		int n = sc.nextInt();
 		
-		double[] vetor = new double[n];
-		
+		int[] vetor = new int[n];
 		
 		for (int i = 0; i < vetor.length; i++) {
 			System.out.print("Digite um número: ");
-			vetor[i] = sc.nextDouble();			
+			vetor[i] = sc.nextInt();
 		}
+
 		
-		double media = 0;
+		double somaPares = 0;
+		double qtdPares = 0;
+		double mediaPares = 0;
 		for (int i = 0; i < vetor.length; i++) {
-			media += vetor[i] / vetor.length;
 			
-		}
-		
-		System.out.println("MÉDIA DO VETOR = "+ media);
-		System.out.println("ELEMENTOS ABAIXO DA MÉIDA:");
-		
-		for (int i = 0; i < vetor.length; i++) {			
-			if(vetor[i] < media) {
-				System.out.println(" " + vetor[i] );
+			if (vetor[i] % 2 == 0) {				
+//				somaPares += vetor[i];
+				qtdPares++;
+				mediaPares = (somaPares += vetor[i]) / qtdPares;
+				
 			}
 		}
 		
+		if(mediaPares != 0) {
+			System.out.println("MÉDIA DOS PARES "+ mediaPares);
+			
+		}else{
+			System.out.println("NENHUM NÚMERO É PAR");
+		}
 		
 		
 		
 		
-		sc.close();
-
 	}
 
 }
