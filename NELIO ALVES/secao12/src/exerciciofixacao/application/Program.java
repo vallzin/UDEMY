48ee7acbed22ff15b0f8f9ca41dee38156fa72fa
vallzin <valmilson@gmail.com>
2024-11-19 18:52:56 -0300

    seção12 exercício de fixação

---
+++ NELIO ALVES/secao12/src/exerciciofixacao/application/Program.java
@@ -0,0 +1,76 @@
+package exerciciofixacao.application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import exerciciofixacao.entities.Order;
+import exerciciofixacao.entities.enums.OrderStatus;
+import exerciciofixacao.entities.Client;
+import exerciciofixacao.entities.OrderItem;
+import exerciciofixacao.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Enter cliente data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("birth date (DD/MM/YYYY):  ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		
+		
+		for (int i = 0; i <n; i++) {
+			System.out.println("Enter #"+ (i + 1) +" item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			sc.nextLine();
+			System.out.print("Qauntity");
+			int quantity = sc.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			
+			OrderItem it = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(it);
+			
+		}
+		
+		System.out.println(order);
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
