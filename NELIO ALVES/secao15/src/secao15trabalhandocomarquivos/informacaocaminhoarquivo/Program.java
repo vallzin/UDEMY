e5b59f3a684b6fd840cc67ce5013a0d7bf103729
vallzin <valmilson@gmail.com>
2024-11-29 18:54:02 -0300

    informação caminho do arquivo

---
+++ NELIO ALVES/secao15/src/secao15trabalhandocomarquivos/informacaocaminhoarquivo/Program.java
@@ -0,0 +1,22 @@
+package secao15trabalhandocomarquivos.informacaocaminhoarquivo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+
+	}
+
+}
