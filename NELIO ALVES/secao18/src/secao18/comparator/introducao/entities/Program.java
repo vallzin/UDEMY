d17ac06d192519c7f595168988f9b62d23f93676
vallzin <valmilson@gmail.com>
2024-12-09 08:25:32 -0300

    seção18 comparator default list

--- NELIO ALVES/secao18/src/secao18/comparator/introducao/entities/Program.java
+++ NELIO ALVES/secao18/src/secao18/comparator/introducao/entities/Program.java
@@ -1,6 +1,8 @@
 package secao18.comparator.introducao.entities;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import secao18.comparator.introducao.application.Product;
@@ -15,7 +17,26 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+//		Comparator<Product> comp = new Comparator<Product>() {
+//
+//			@Override
+//			public int compare(Product p1, Product p2) {
+//				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+//			}
+//			
+//		};
+		
+//		Comparator<Product> comp = (p1, p2) -> {
+//			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+//		};
+//		
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+		
+		
+//		Collections.sort(new Comparator();
+//		list.sort(comp);
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for(Product p : list) {
 			System.out.println(p);
