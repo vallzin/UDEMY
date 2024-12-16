910265068bcab08fc4aee7c12dea8ac9af360761
vallzin <valmilson@gmail.com>
2024-12-16 16:21:28 -0300

    seção18 projetodao 'findAll implementation'

--- NELIO ALVES/projetodao/src/application/Program.java
+++ NELIO ALVES/projetodao/src/application/Program.java
@@ -15,19 +15,26 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println();
-		System.out.println("------ *** TESTE 1 findById *** -----");
+		System.out.println("------ *** TEST 1 findById *** -----");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		System.out.println();
-		System.out.println("------ *** TESTE 2 findByDepartment *** -----");
+		System.out.println("------ *** TEST 2 findByDepartment *** -----");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+			
+		System.out.println();
+		System.out.println("------ *** TEST 3 findAll *** -----");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+			
+		}
 
 	}
-
 }
