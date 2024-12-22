3c1547acc168aab42ed91d76babb3883334767d8
vallzin <valmilson@gmail.com>
2024-12-22 17:44:29 -0300

    OrderStatus enum

--- NELIO ALVES/course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ NELIO ALVES/course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maira Brown", "maria@email.com", "9898989898", "123456");
 		User u2 = new User(null, "Alex Green", "alex@email.com", "878787878", "654321");
 		
-		Order o1 = new Order(null, Instant.parse("2024-12-20T16:46:55Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2024-11-15T15:26:15Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2024-11-10T14:30:05Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2024-12-20T16:46:55Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2024-11-15T15:26:15Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2024-11-10T14:30:05Z"), OrderStatus.PAID, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
